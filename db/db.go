089c5896ee560ff6f4f1b027da467593e620429a
Sergio Robles <srlps2702@gmail.com>
2020-08-27 18:40:41 -0500

    Interface definition modified and added comments

--- db/db.go
+++ db/db.go
@@ -4,13 +4,25 @@ Package db exposes a very simple database with different implementations
 package db
 
 /*
-Db defines the base interface for using de database
+Db defines the base interface for using de database. Most of the methods return a bool that indicates the success of the operation.
 */
 type Db interface {
+
+	// Connect creates the database connection
 	Connect() bool
+
+	// Disconnect closes the database connection
 	Disconnect()
-	Create(interface{}) bool
-	Retrieve(string) bool
+
+	// Create adds a new object to the database and returns the id of the newly created object
+	Create(interface{}) (string, bool)
+
+	// Retrieve returns an object in the database by it's id
+	Retrieve(string) (interface{}, bool)
+
+	// Update replaces an object in the database identified with the provided id with another object
 	Update(string, interface{}) bool
+
+	// Delete removes and object from the database by it's id
 	Delete(string) bool
 }
